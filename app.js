31a63842581a8ef9f573d670383b9c24485a1ae4
Francois ROLAND <froland@froland.be>
2020-11-28 18:58:57 +0100

    Set and read cookies.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -16,11 +15,10 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('0b649762-66c1-45b1-9830-549506523722'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cookie/set', (req, res, next) => {
+  res.cookie('mycookie', '1234');
+  res.render('cookieSet', { title: 'Cookie setter' });
+});
+
+router.get('/cookie/read', (req, res, next) => {
+  const cookieValue = req.cookies.mycookie;
+  res.render('cookieRead', { title: 'Cookie reader', cookieValue });
+});
+
 module.exports = router;
