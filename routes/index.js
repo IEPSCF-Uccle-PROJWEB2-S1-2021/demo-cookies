3a63870072d3d5c51e982b352d07dc21eb5bd8b8
Francois ROLAND <froland@froland.be>
2020-11-29 10:25:21 +0100

    Cleanup.

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,11 @@ const router = new express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  const themeFlavor = req.signedCookies.theme;
-  res.render('index', { title: 'Express', theme: themeFlavor });
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/anotherPage', (req, res, next) => {
-  const themeFlavor = req.signedCookies.theme;
-  res.render('anotherPage', { title: 'Another webpage', theme: themeFlavor });
-});
-
-const DAY_AS_MS = 24 * 60 * 60 * 1000;
-
-router.post('/theme', (req, res, next) => {
-  const themeFlavor = req.body.flavor;
-  res.cookie('theme', themeFlavor, {
-    maxAge: 100 * DAY_AS_MS,
-    httpOnly: true,
-    sameSite: 'Strict',
-    signed: true,
-  });
-  res.redirect('/');
+  res.render('anotherPage', { title: 'Another webpage' });
 });
 
 module.exports = router;
